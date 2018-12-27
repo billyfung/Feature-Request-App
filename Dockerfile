171b54576b9ebf41e7ab0700c3621de6fcfdbe58
Billy Fung <billy@billyfung.com>
2018-12-27 22:53:24 +1300

    basic docker-compose set up with barebones form + request route

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.6-alpine
-
-COPY . /app
-WORKDIR /app
-RUN pipenv install --dev
-ENTRYPOINT ["python"]
-CMD ["app.py"]
---
+++ app.py
@@ -0,0 +1,57 @@
+import datetime
+import os
+
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.secret_key = os.environ['APP_SECRET_KEY']
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class FeatureRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    client = db.Column(db.String(), nullable=False)
+    client_priority = db.Column(db.Integer, nullable=False)
+    target_date = db.Column(db.Date, nullable=False)
+    product_area = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Description %r>' % self.description
+
+class RequestForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
+    client = StringField('client', validators=[DataRequired()])
+    client_priority = IntegerField('priority', validators=[DataRequired()])
+    target_date = DateField('date')
+    product_area = StringField('product area')
+
+
+@app.route("/", methods=('GET', 'POST'))
+def feature_request():
+    form = RequestForm()
+    if form.validate_on_submit():
+        feat_request = FeatureRequest(
+            name=form.name.data,
+            email=form.email.data,
+            date_signed_up=datetime.datetime.now()
+        )
+        db.session.add(feat_request)
+        db.session.commit()
+        return redirect(url_for('success'))
+    return render_template('request.html', form=form)
+
+@app.route("/success")
+def success():
+    return "Request noted!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
